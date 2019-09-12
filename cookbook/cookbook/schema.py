be157cda9bde1b5e669188409a82e1d07a2aa3ca
Amit Thapa <amithapa1994@gmail.com>
2019-09-12 09:12:52 +0530

    Cook boot

--- cookbook/cookbook/schema.py
+++ cookbook/cookbook/schema.py
@@ -2,9 +2,18 @@ import graphene
 
 import cookbook.ingredients.schema
 
-class Query(cookbook.ingredients.schema.Query, graphene.ObjectType):
+class Query(
+    cookbook.ingredients.schema.Query,
+    graphene.ObjectType
+):
     # THis class will inherit from multiple Queries
     # as we begin to add more apps to our project
     pass
 
-schema = graphene.Schema(query=Query)
+class Mutation(
+    cookbook.ingredients.schema.Mutation,
+    graphene.ObjectType
+):
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
